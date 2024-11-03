9f8617d9c5e70dee8d09012c709665580eeed35c
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2024-11-03 12:00:29 +0200

    finishing up the code

--- README.md
+++ README.md
@@ -4,35 +4,56 @@
 
 * [Description](#description)
 * [Technologies](#technologies)
+* [Features](#features)
 * [Installation](#installation)
 * [Contributing](#contributing)
 * [License](#license)
 
 ## Description
-This project is a simple landing page that uses JavaScript to create a dynamic navigation bar that updates based on the sections of the page. The navigation bar is created dynamically based on the sections of the page. When a section is in the viewport, the corresponding navigation item is highlighted. The navigation bar also scrolls smoothly to the corresponding section when clicked.
+
+This project is a simple landing page that uses JavaScript to create a dynamic navigation bar that updates based on the
+sections of the page. The navigation bar is created dynamically based on the sections of the page. When a section is in
+the viewport, the corresponding navigation item is highlighted. The navigation bar also scrolls smoothly to the
+corresponding section when clicked.
 
 ## Technologies
+
 This project was created using the following technologies:
+
 - HTML
 - CSS
 - JavaScript
 
+# Features
+
+- Navigation bar is created dynamically based on the sections of the page
+- Navigation bar scrolls smoothly to the corresponding section when clicked
+
 ## Installation
+
 To run this project, download the files and open the `index.html` file in a web browser.
+
 ### Instructions
+
 1. Clone the repository
+
 ```
 git clone https://github.com/dana-akesh/Udacity-Landing-Page
 ```
+
 2. Open the `index.html` file in a web browser
 
 ## Contributing
 
-This project is from Udacity's Front End Web Developer Nanodegree program. Therefore, I will not be accepting contributions.
+This project is from Udacity's Front End Web Developer Nanodegree program. Therefore, I will not be accepting
+contributions.
 
 ## License
 
-This project includes content provided under a license from Udacity, Inc. (© 2012 - 2020). The educational materials, such as code samples and assignments, are licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License. This means:
+This project includes content provided under a license from Udacity, Inc. (© 2012 - 2020). The educational materials,
+such as code samples and assignments, are licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0
+License. This means:
+
 - You may use, share, and reference the content for personal, non-commercial purposes.
 - Modifications and commercial use are not permitted without explicit permission.
 
--- js/app.js
+++ js/app.js
@@ -1,62 +1,93 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
+ */
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
+// global variables for the navigation bar and the sections
+const navBar = document.getElementById('navbar__list');
+const pageSections = document.querySelectorAll('section');
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
+const navBuilder = () => {
+    let navUI = document.createDocumentFragment(); // Create a fragment to improve performance
+    pageSections.forEach(section => {
+        const sectionID = section.id;
+        const sectionDataNav = section.dataset.nav;
 
+        // Create each list item
+        const createList = document.createElement('li');
+        createList.innerHTML = `<a class="menu__link" href="#${sectionID}">${sectionDataNav}</a>`;
+
+        // Append the list item to the fragment
+        navUI.appendChild(createList);
+    });
+
+    // Append the fragment to the navBar after the loop
+    navBar.appendChild(navUI);
+};
+
+// call the function to build the nav
+navBuilder();
 
 // Add class 'active' to section when near top of viewport
+const VALUE = 150;
 
+function makeActive() {
+    for (const section of pageSections) {
+        const box = section.getBoundingClientRect();
+        //Find a value that works best, but 150 seems to be a good start.
+        if (box.top <= VALUE && box.bottom >= VALUE) {
+            section.classList.add('active');
+            // change the heading color
+            const heading = section.querySelector('h2');
+            heading.style.color = '#000000';
+        } else {
+            section.classList.remove('active');
+            // change the heading color
+            const heading = section.querySelector('h2');
+            heading.style.color = '#ffffff';
+        }
+    }
+}
+
+document.addEventListener('scroll', function () {
+        makeActive();
+    }
+);
 
 // Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
+function scrollToClick() {
+    navBar.addEventListener('click', function (event) {
+        event.preventDefault();
+        if (event.target.nodeName === 'A') {
+            const clickedSection = document.querySelector(event.target.hash);
+            clickedSection.scrollIntoView({behavior: "smooth"});
+        }
+    });
+}
 
 // Scroll to section on link click
-
-// Set sections as active
-
+scrollToClick();
 
